10031ab0d3de26021ae1a600e3e0f74d58c053b3
gunpan <panschur@hotmail.de>
2026-08-22 10:29:38 +0200

    Auto re-join room on socket reconnect and sync room state on tab visibility change

--- client/js/socketClient.js
+++ client/js/socketClient.js
@@ -6,9 +6,13 @@ class SocketClient {
   constructor() {
     // Dynamic URL resolution: works on localhost and on Render automatically
     const socketUrl = window.location.origin;
+    this.currentRoomCode = null;
+    this.currentPlayerName = null;
+
     this.socket = io(socketUrl, {
       reconnection: true,
-      reconnectionAttempts: 5
+      reconnectionAttempts: 10,
+      reconnectionDelay: 1000
     });
 
     this.onGameStateCallback = null;
@@ -20,6 +24,10 @@ class SocketClient {
   initListeners() {
     this.socket.on('connect', () => {
       console.log('[Socket] Connected to server:', this.socket.id);
+      if (this.currentRoomCode && this.currentPlayerName) {
+        console.log(`[Socket] Auto-rejoining room ${this.currentRoomCode} as ${this.currentPlayerName}`);
+        this.joinRoom(this.currentRoomCode, this.currentPlayerName, () => {});
+      }
     });
 
     this.socket.on('game_state', (state) => {
@@ -37,6 +45,18 @@ class SocketClient {
     this.socket.on('disconnect', () => {
       console.warn('[Socket] Disconnected from server');
     });
+
+    // Auto-sync state when browser tab becomes active again
+    document.addEventListener('visibilitychange', () => {
+      if (document.visibilityState === 'visible' && this.currentRoomCode && this.currentPlayerName) {
+        console.log('[Socket] Tab active - refreshing room state...');
+        if (!this.socket.connected) {
+          this.socket.connect();
+        } else {
+          this.joinRoom(this.currentRoomCode, this.currentPlayerName, () => {});
+        }
+      }
+    });
   }
 
   onGameState(callback) {
@@ -48,11 +68,21 @@ class SocketClient {
   }
 
   createRoom(playerName, callback) {
-    this.socket.emit('create_room', { playerName }, callback);
+    this.currentPlayerName = playerName;
+    this.socket.emit('create_room', { playerName }, (res) => {
+      if (res && res.success) {
+        this.currentRoomCode = res.roomCode;
+      }
+      if (typeof callback === 'function') callback(res);
+    });
   }
 
   joinRoom(roomCode, playerName, callback) {
-    this.socket.emit('join_room', { roomCode, playerName }, callback);
+    this.currentRoomCode = roomCode;
+    this.currentPlayerName = playerName;
+    this.socket.emit('join_room', { roomCode, playerName }, (res) => {
+      if (typeof callback === 'function') callback(res);
+    });
   }
 
   toggleReady() {
